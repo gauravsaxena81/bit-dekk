5c770b97cdf711e504ed43074e9288b83ae5682c
mishra9manish@gmail.com <mishra9manish@gmail.com@205f2c20-a07d-3ca8-0392-aa95d3fc9e6b>
2012-11-01 07:52:00 +0000

    dimension value sequence

---
+++ trunk/bitdekk/src/org/bitdekk/util/DimensionValueIDGenerator.java
@@ -0,0 +1,57 @@
+package org.bitdekk.util;
+
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import org.bitdekk.store.Dimension;
+import org.bitdekk.store.DimensionValue;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/* In future maybe this will be persisted. But since everything is in memory right now, everything goes
+ * down when the system crashes or when main exits.
+ */
+public class DimensionValueIDGenerator{
+	HashMap<Dimension,HashMap<String,Integer>> dimensionSpace;
+	HashMap<Dimension,AtomicInteger> dimensionIDSequence;
+	
+	public DimensionValueIDGenerator(){
+		dimensionSpace = new HashMap<Dimension,HashMap<String,Integer>>();
+		dimensionIDSequence = new HashMap<Dimension, AtomicInteger>();
+	}
+	
+	/*
+	 * crappy code to be refactored
+	 */
+	
+	public int getID(Dimension dim, String dimValue){
+		//id already exists for the dimension value
+		if(dimensionSpace.containsKey(dim)&& dimensionSpace.get(dim).containsKey(dimValue)){
+			return dimensionSpace.get(dim).get(dimValue);
+		}
+		
+		// dimension exists but dimension value does not exist
+		if(dimensionSpace.containsKey(dim) && !dimensionSpace.get(dim).containsKey(dimValue)){
+			// get a new ID from the sequence
+			// since dimensionSpace has the dimension it should also be present in the Sequence
+			int i = dimensionIDSequence.get(dim).addAndGet(1);
+			// add above to the dimension space
+			dimensionSpace.get(dim).put(dimValue, i);
+			return i;
+		}
+		
+		if(!dimensionSpace.containsKey(dim)){
+			assert !dimensionIDSequence.containsKey(dim);
+			dimensionIDSequence.put(dim, new AtomicInteger(0));
+			int i = dimensionIDSequence.get(dim).addAndGet(1);
+			HashMap<String,Integer> t = new HashMap<String, Integer>();
+			t.put(dimValue, i);
+			dimensionSpace.put(dim, t);
+			return i;
+		}
+		
+		System.out.println("If you are getting here something is wrong");
+
+		return 0;
+	}
+}
