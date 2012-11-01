5d67df9b0c6021586a89569084f59f5ad90a4fe9
mishra9manish <mishra9manish@gmail.com>
2012-11-01 12:18:03 +0000

    None

---
+++ bitdekk/test/org/bitdekk/test/store/TestMeasure.java
@@ -0,0 +1,32 @@
+package org.bitdekk.test.store;
+
+import java.io.FileReader;
+
+import au.com.bytecode.opencsv.*;
+import java.io.FileReader;
+import org.bitdekk.store.Measure;
+
+public class TestMeasure {
+
+	private static final String FILE_ADDRESS = "E:/EclipseWorkspace/manish.mishra/workspace/BitDekk/test/org/bitdekk/test/store/Test.csv";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception{
+		CSVReader reader = new CSVReader(new FileReader(FILE_ADDRESS));
+		String[] nextLine;
+		
+		nextLine = reader.readNext();
+		
+		//Read the measure Columns
+		Measure mesA = new Measure();
+		mesA.setName(nextLine[4]);
+		
+		Measure mesB = new Measure();
+		mesB.setName(nextLine[5]);
+		
+		System.out.println("Measure A:" + mesA.getName());
+		System.out.println("Measure B:" + mesB.getName());
+	}
+
+}
